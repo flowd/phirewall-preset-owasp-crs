12cd26fe104038242a78f088339f268bed274515
Sascha Egerer <sascha.egerer@flowd.de>
2026-09-08 15:23:50 +0200

    Pass the request to exclusion conditions and manipulators

--- src/Engine/Variable/CallableTargetExclusionCondition.php
+++ src/Engine/Variable/CallableTargetExclusionCondition.php
@@ -4,21 +4,25 @@ declare(strict_types=1);
 
 namespace Flowd\PhirewallPresetOwaspCrs\Engine\Variable;
 
+use Psr\Http\Message\ServerRequestInterface;
+
 /**
- * Adapts a closure `fn (string $value, ?string $name, string $variable): bool`
- * to {@see TargetExclusionConditionInterface}.
+ * Adapts a closure `fn (string $variable, ?string $name, string $value,
+ * ServerRequestInterface $serverRequest): bool` to
+ * {@see TargetExclusionConditionInterface}. A closure declaring fewer
+ * parameters ignores the rest.
  */
 final readonly class CallableTargetExclusionCondition implements TargetExclusionConditionInterface
 {
     /**
-     * @param \Closure(string, ?string, string): bool $closure
+     * @param \Closure(string, ?string, string, ServerRequestInterface): bool $closure
      */
     public function __construct(private \Closure $closure)
     {
     }
 
-    public function shouldExclude(string $value, ?string $name, string $variable): bool
+    public function shouldExclude(string $variable, ?string $name, string $value, ServerRequestInterface $serverRequest): bool
     {
-        return ($this->closure)($value, $name, $variable);
+        return ($this->closure)($variable, $name, $value, $serverRequest);
     }
 }
